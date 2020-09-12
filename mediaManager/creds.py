3475f05bc53e93e0998bccdfa4f5cd99fbb12ff5
hrk2023 <hrkdasgupta025@gmail.com>
2020-09-12 23:44:18 +0530

    New Deploy 1

--- mediaManager/creds.py
+++ mediaManager/creds.py
@@ -1,7 +1,9 @@
 import os
 from pymongo import MongoClient
-# DATABASE_URI = os.environ.get('DATABASE_URI',"default_value")
-cluster = MongoClient('mongodb+srv://admin:nydqqzuy1324@cluster0.oobol.mongodb.net/')
+DATABASE_URI = os.environ.get('DATABASE_URI',"default_value")
+DATABASE_URI_TWO = os.environ.get('DATABASE_URI_TWO',"default_value")
+
+cluster = MongoClient(DATABASE_URI)
 db = cluster.eceSite
-cluster2 = MongoClient('mongodb+srv://admin:nydqqzuy1324@cluster0.fglir.mongodb.net/')
+cluster2 = MongoClient(DATABASE_URI_TWO)
 db2 = cluster2.ecesite
