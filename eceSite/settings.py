cbecf6ea53250cc04bf4c032fb133a7918d222c1
hrk2023 <hrkdasgupta025@gmail.com>
2020-09-13 00:34:09 +0530

    Deploy 5

--- eceSite/settings.py
+++ eceSite/settings.py
@@ -26,7 +26,7 @@ if os.path.isfile(dotenv_file):
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY',"default_value")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
